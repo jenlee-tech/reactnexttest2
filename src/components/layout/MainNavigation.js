e0142763198affaf1bb8b770ddc350c3a4ce9552
Jennifer Lee <jenlee.tech@gmail.com>
2022-07-07 23:56:05 -0400

    added Links

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function MainNavigation() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
