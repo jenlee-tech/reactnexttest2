d7b2079ff261505b6ba9141455b3073d02716bf2
Jennifer Lee <jenlee.tech@gmail.com>
2022-07-31 00:16:10 -0400

    added props in meetuplist to pass to meetupitem

---
+++ src/components/meetups/MeetupList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./MeetupList.module.css";
+import MeetupItem from "./MeetupItem";
+
+export default function MeetupList(props) {
+  return (
+    <div>
+      <ul className={classes.list}>
+        {props.meetups.map((meetup) => (
+          <MeetupItem
+            key={meetup.id}
+            id={meetup.id}
+            image={meetup.image}
+            title={meetup.title}
+            description={meetup.description}
+            address={meetup.address}
+          />
+        ))}
+      </ul>
+      MeetupList
+    </div>
+  );
+}
