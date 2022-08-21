3f59231338de8ff18e362e5421a1e0aee525de88
Jennifer Lee <jenlee.tech@gmail.com>
2022-08-21 00:50:16 -0400

    added fetch command for request

--- src/pages/NewMeetupsPage.js
+++ src/pages/NewMeetupsPage.js
@@ -1,10 +1,20 @@
 import NewMeetUpForm from "../components/meetups/NewMeetUpForm";
 
 function NewMeetupsPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://my-first-firebase-projec-9248d-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "content-type": "application/json" },
+      }
+    );
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetUpForm />
+      <NewMeetUpForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
