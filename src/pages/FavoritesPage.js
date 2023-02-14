409de90d010cbb1b50c2ab3e5a86d05043318cee
Jennifer Lee <jenlee.tech@gmail.com>
2023-02-14 09:04:24 -0500

    imported context and meetuplist in favoritespage

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
 
 function FavoritesPage(props) {
-  return <div>Favorites - just want to make sure</div>;
+  const favoirtesCtx = useContext(FavoritesContext);
+  return (
+    <section>
+      <h1> My Favoirtes </h1>
+      <MeetupList meetups={favoirtesCtx.favorites} />
+    </section>
+  );
 }
 
 export default FavoritesPage;
